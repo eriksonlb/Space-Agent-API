05d9bc3c6c4298745d51c3901b7d4a2674137f26
Erikson Lopes <elbezerra@topaz.com.br>
2021-05-06 18:08:17 -0300

    first commit

---
+++ web_scrapping.py
@@ -0,0 +1,35 @@
+import requests
+import datetime
+
+from bs4 import BeautifulSoup
+
+def get_data():
+    response = requests.get('https://www.timeanddate.com/astronomy/sights-to-see.html')
+    content = response.content
+    today = datetime.datetime.now()
+    month = today.month
+    months = {
+        1: 'january',
+        2: 'february',
+        3: 'mars',
+        4: 'april',
+        5: 'may',
+        6: 'june',
+        7: 'july',
+        8: 'august',
+        9: 'september',
+        10: 'october',
+        11: 'november',
+        12: 'december'
+    }
+
+
+    site = BeautifulSoup(content, 'html.parser')
+    events = site.findAll('article', attrs={'class': 'post-row'})
+    for event in events:
+        info = event.find('h3').text
+        date = info.split(':')[0]
+        title = info.split(':')[1]
+        print(f'{date} : {title}')
+
+get_data()
