ac087bd1d1752cb1892f4ca4eb707c21752f24b2
Erikson Lopes <elbezerra@topaz.com.br>
2021-05-07 00:44:14 -0300

    Alterações para deploy

--- app.py
+++ app.py
@@ -2,15 +2,20 @@ from flask import Flask, jsonify
 import json
 
 from helpers import data_file_name
+import os
 
 app = Flask(__name__)
 
-@app.route("/data", methods=["GET"])
+@app.route("/", methods=["GET"])
 def get_space_data():
     with open(f"data/{data_file_name()}.json", "r", encoding="utf8") as json_file:
             data = json.load(json_file)
             json_file.close()
             return jsonify(data)
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
 
 
 app.run()
